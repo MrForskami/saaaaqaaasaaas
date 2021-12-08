1f64bc6a59388cb0fa634c82fc2952e332425eee
Iepichnieyiighr <95309916+MrForskami@users.noreply.github.com>
2021-12-08 16:57:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+s = "ttttuuuurrrrttttlllleeee"
+print(s[1])
+print(s[5])
+print(s[10])
+print(s[14])
+print(s[18])
+print(s[20])
+import time
+print ("2 часть")
+time.sleep(1)
+d = 2
+print("два плюс два")
+print(d + d)
